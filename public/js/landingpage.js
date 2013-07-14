affaf16be9810ddc715ef1a5be8a8b4141aa0f08
Matej Kramny <matejkramny@gmail.com>
2013-07-14 20:31:40 +0100

    Fixed CSS fixed position
    Fixes footer and navbar..

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -2,4 +2,22 @@ $("document").ready(function() {
 	$("#client, #artist").click(function() {
 		window.location = $(this).attr("href")
 	});
+	
+	$(".client, .artist").mousemove(function(event) {
+		var $client = $(".client");
+		var $artist = $(".artist");
+		var artWidth = $artist.width();
+		var coord = event.pageX;
+		if (coord < artWidth) {
+			var diff = (artWidth - coord) / artWidth;
+			$artist.css({
+				"box-shadow": "inset -"+(2+diff)+"px 0 "+(3+diff*6)+"px #000"
+			})
+		} else {
+			var diff = (coord - artWidth) / artWidth;
+			$client.css({
+				"box-shadow": "inset "+(2+diff)+"px 0 "+(3+diff*6)+"px #000"
+			})
+		}
+	});
 })
